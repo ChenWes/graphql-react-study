1ac9aab7faa592dfc4443935f3cb72b5eed145b9
ChenWes <chenxuhua0530@163.com>
2018-08-13 15:02:06 +0800

    lesson 31
    * 同一个组件有两个graphql查询，需要使用compose合并它，并且在列表中，会需要修改this.props的名称获取属性

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getAuthorsQuery } from '../queries/queries';
+import { graphql, compose } from 'react-apollo';
+import { getAuthorsQuery, addBookMutation } from '../queries/queries';
 
 class AddBook extends Component {
     constructor(props) {
@@ -13,7 +13,8 @@ class AddBook extends Component {
     }
 
     displayAuthors() {
-        var data = this.props.data;
+        var data = this.props.getAuthorsQuery;
+        console.log(this.props);
         if (data.loading) {
             return (<option>Loading Authors...</option>)
         } else {
@@ -71,4 +72,7 @@ class AddBook extends Component {
     }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, { name: 'getAuthorsQuery' }),
+    graphql(addBookMutation, { name: 'addBookMutation' })
+)(AddBook);
